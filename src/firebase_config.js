95e37731455ab941f41dcbfbdd29feb6ee4c07a2
Esteban Saravia <estebansaravia@gmail.com>
2018-04-25 14:01:57 -0300

    removing configuration file

--- src/firebase_config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export default {
-  apiKey: "your data",
-  authDomain: "your data",
-  databaseURL: "your data",
-  projectId: "your data",
-  storageBucket: "your data",
-  messagingSenderId: "your data"
-};
